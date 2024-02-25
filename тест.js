9be7a5a598bd98044107afbbfc65bbfd7008fa85
Fivalin <ivanfilipenkov5@gmail.com>
2024-02-25 12:10:03 +0300

    Mendel
    Mendel

---
+++ тест.js
@@ -0,0 +1,48 @@
+var falses = 0
+var trues = 0
+
+function True() {
+  let a =document.getElementById('Btext').value = 'Правильно, Дмитрий Менделеев был 17-м ребёнком в большой семье.';
+  document.getElementById('Btext').innerHTML = a; 
+}
+function False() {
+  let a =document.getElementById('Btext').value = 'Неверно, Дмитрий Менделеев был 17-м ребёнком в большой семье.';
+  document.getElementById('Btext').innerHTML = a; 
+  }
+  function True2() {
+    let a =document.getElementById('Btext').value = 'Да, Менделеев действительно был оставлен на второй год из-за плохой успеваемости.';
+    document.getElementById('Btext').innerHTML = a; 
+  }
+  function False2() {
+    let a =document.getElementById('Btext').value = 'Неверно, Менделеев был оставлен на второй год из-за плохой успеваемости.';
+    document.getElementById('Btext').innerHTML = a; 
+    }
+    function True3() {
+      let a =document.getElementById('Btext').value = 'Да, Менделеев действительно не изобретал водку.';
+      document.getElementById('Btext').innerHTML = a; 
+    }
+    function False3() {
+      let a =document.getElementById('Btext').value = 'Менделеев не изобретал водку! Это заблуждение возникло на основе труда о соединении воды и спирта, в момент публикации которого водка уже была изобретена.';
+      document.getElementById('Btext').innerHTML = a; 
+    }
+    function True4() {
+      let a =document.getElementById('Btext').value = 'Действительно, Менделеев очень любил музыку Бетховена.';
+      document.getElementById('Btext').innerHTML = a; 
+    }
+    function False4() {
+      let a =document.getElementById('Btext').value = 'Неверно, Менделеев очень любил музыку Бетховена.';
+      document.getElementById('Btext').innerHTML = a; 
+      }
+      function True5() {
+        let a =document.getElementById('Btext').value = 'Да, Менделеев действительно предпочитал лечиться народными способами.';
+        document.getElementById('Btext').innerHTML = a; 
+      }
+      function False5() {
+        let a =document.getElementById('Btext').value = 'Неверно, Менделеев действительно не любил современную медицину.';
+        document.getElementById('Btext').innerHTML = a; 
+        }      
+
+
+
+
+
